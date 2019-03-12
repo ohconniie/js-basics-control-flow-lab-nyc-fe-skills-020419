2c5c5f8f5b7d995897bb713701ed9a818122d6c6
Connie Chan <conniechan.10@gmail.com>
2019-03-12 00:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function scuberGreetingForFeet(someDistance) {
 if (someDistance <= 400) {
   return 'This one is on me!'
 } else if
-  (someDistance > 2000) {
+  (someDistance > 2000 && someDistance < 2500) {
     return 'I will gladly take your thirty bucks.'
-  } else if (someDistance < 2500) {
+  } else if (someDistance > 2500) {
     return 'No can do.'
   }
 }
