be5d61a1900c0b1f9f545d02d7d111a156ae29fc
Connie Chan <conniechan.10@gmail.com>
2019-03-12 00:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,7 @@ function switchOnCharmFromTip(messageTip) {
       return  'Thank you so much.';
     case 'not as generous':
       return  'Thank you.';
+    case 'not as generous':
+      return  'Thank you.';
       
   }
