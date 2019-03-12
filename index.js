da9a263fda230c23b677567f4cb66b42387852ef
Connie Chan <conniechan.10@gmail.com>
2019-03-12 00:12:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someDistance) {
+if (someDistance <= 400) {
+  return 'This one is on me!'
+} else if
+  (someDistance > 2000 && someDistance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someDistance > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(someCity) {
+if (someCity === 'NYC') {
+  return  'Ok, sounds good.'
+} else {
+  return 'No go.'
+}
+}
+
+function switchOnCharmFromTip(messageTip) {
+  switch(messageTip) {
+    case 'generous':
+      return  'Thank you so much.';
+    case 'not as generous':
+      return  'Thank you.';
+    case 'thanks for everything':
+      return  'Bye.';
+  }
+  }
